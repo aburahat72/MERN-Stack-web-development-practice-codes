255a2142d68eaa1b0eb555d9271bd528cee31890
Abu Rahat Barbhuiya <142603269+aburahat72@users.noreply.github.com>
2025-10-08 17:42:15 +0530

    Add files via upload

---
+++ Javascript/Js variables and datatypes/script.js
@@ -0,0 +1,10 @@
+// const product = {
+//     name: "rahat",
+//     age:25,
+//     istrue:false
+
+// };
+// product["age"] = product["age"] + 5;
+// // product.age = product.age + 1;  , using above and any of the two methods can access the key values
+// console.log(product["age"]);
+// let , var , const
---
+++ Javascript/operators and conditional statement/script.js
@@ -0,0 +1,107 @@
+// Addition
+// let a = 8;
+// let b = 2;
+// console.log("a =",a, "& b =",b);
+// console.log("a + b =", a + b);
+// console.log("a - b =", a - b);
+// console.log("a * b = ", a * b);
+// console.log("a / b =", a / 2);
+// console.log("a % b =", a % b);
+
+//Increment and decrement operator
+// let a, b;
+// a = 5;
+// b = 2;
+// // a = a - 1
+// a--;
+// // b = b + 1
+// b++;
+// console.log("a =", a, "b = ", b);
+// --a , --b pre decrement
+
+// Assignment operator
+// let a = 5;
+// let b = a += 1;
+// console.log("b =", b);
+// let c = b -= 2;
+// console.log("c =", c);
+
+//comparison operator
+// let a = 7;
+// let b = 3;
+
+// console.log(a == b);
+// console.log(a != b);
+// console.log(a===b);
+// console.log(a > b);
+// console.log(a < b);
+// console.log(a <= b);
+// console.log(a >= b);
+
+// Logical operator
+// let a = 5;
+// let b = 7;
+// let cond1 = (a > b);
+// let d = 8;
+// let e = 8;
+// let cond2 = (d===e);
+
+// console.log("cond1 vs cond2 =", cond1 && cond2);
+
+//conditional statement
+// let age = 18;
+// if(age < 21) {
+//     console.log("you can vote");
+// } else {
+//     console.log("not elligible for vote");
+// }
+
+//conditional statement
+// let mode = "dark";
+// let color;
+
+// if (mode === "dark") {
+//     color = "black";
+// } else {
+//     color = "white";
+// }
+// console.log(color);
+
+// let number = 50;
+// if(number % 2 === 0) {
+//     console.log("number is even")
+// } else {
+//     console.log("number is odd")
+// }
+
+//Ternary operator
+// let age = 28;
+// let out = (age >= 18 ? "true" : "false");
+// console.log(out);
+
+// check the number is it multiple of 5
+// let num = prompt("enter number");
+// // let num = 21;
+// if(num % 5 === 0) {
+//     console.log("yes");
+// } else {
+//     console.log("not");
+// }
+
+// to check the grade of the students
+// let marks = prompt("enter the number");
+// if(90 <= marks && marks >= 100){
+//     console.log("A");
+// } else if(89 <= marks) {
+//     console.log("B");
+// } else if (79 <= marks) {
+//     console.log("C");
+// } else if (69 <= marks) {
+//     console.log("D");
+// } else if(59 <= marks){
+//     console.log("fail");
+// }
+let num = 75;
+// if(num <= 100 && num > 100) {
+//     console.log("A")
+// }
