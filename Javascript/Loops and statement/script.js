4bb2b67ebea49c8c99dc9c27fe0a27b2ccea88f2
Abu Rahat Barbhuiya <142603269+aburahat72@users.noreply.github.com>
2025-10-09 08:16:27 +0530

    Add files via upload

---
+++ Javascript/Loops and statement/script.js
@@ -0,0 +1,69 @@
+// for(let i = 1; i < 10; i++) {
+//     console.log(i);
+// }
+
+// // loop for printing the string
+// for(let i = 0; i < 10; i++){
+//     console.log("Abu Rahat");
+// }
+
+// calculate the sum of the number 1 to n
+// let num = 5;
+// let count = 0;
+// for(let i = 0; i < num; i++){
+// count = count + i;
+// }
+// console.log(count);
+
+// to print the even number from to 1 to 100 using js
+// let num = 0;
+// for (let i = 1; i < 100; i++) {
+//   if (i % 2 === 0) {
+//     console.log(i);
+
+//   }
+//   console.log()
+// }
+
+// To take the user input a random number game if they match with computer print "congrulations you have win"
+// let computernumber = 99;
+// let userinput = prompt("Enter the numbers");
+// while(computernumber != userinput){
+//     userinput = prompt("guess again");
+// }
+// console.log("you have win");
+
+// Another method iam trying
+// let cp = 54;
+// let num = prompt("enter the number");
+// while(num != cp) {
+//     num = prompt("reentered the number");
+// }
+// if(num != cp){
+//     console.log("not win");
+// } else {
+//     console.log("you win");
+// }
+
+// To print the position of the string
+// let str = 'Rahat';
+// console.log(str[2]);
+
+//Template literals
+// let specialstring = 'This is a template literal';
+// console.log(specialstring);
+// console.log(typeof specialstring);
+
+// for of only returns tthe indices
+// it will return values and keys for in
+
+
+// const person = {
+//   name: 'Alice',
+//   age: 30,
+//   city: 'Paris'
+// };
+
+// for (const key in person) {
+//   console.log(`${key}: ${person[key]}`);
+// }
