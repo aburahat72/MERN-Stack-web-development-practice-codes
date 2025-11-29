a615cb57c49b47aea2367e5326665f3f13443bae
Abu Rahat Barbhuiya <142603269+aburahat72@users.noreply.github.com>
2025-11-29 23:31:44 +0530

    Add files via upload

---
+++ Javascript/Events in Js/script.js
@@ -0,0 +1,20 @@
+// let btn = document.querySelector("#btn");
+// btn.onclick = () => {
+//     console.log("welcome");
+//     for(let i = 0; i < btn.length; i++) {
+//          console.log(i)
+
+//     }
+// }
+
+// let btn = document.querySelector("#btn");
+// btn.onclick = () => {
+//     let a = 25;
+//     a++;
+//     console.log(a);
+// }
+
+let btn = document.querySelector("#btn");
+btn.onmouseover = () => {
+    console.log("you are inside div");
+};
